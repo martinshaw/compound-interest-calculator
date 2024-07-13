0c0fa92ca9576e65ce379a04af4bb00f57f34b0a
Martin Shaw <hello@martinshaw.co>
2024-07-14 00:39:29 +0100

    Testing deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: isProduction ? '/compound-interest-calculator' : undefined,
+    assetPrefix: '/compound-interest-calculator',
     distDir: 'dist',
 };
 
